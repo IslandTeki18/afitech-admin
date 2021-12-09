b9e11a1c90bf405cd650475455384d88ea7a9eae
Landon McKell <landon.roney7923@gmail.com>
2021-12-09 14:03:14 -0700

    Task: Built Header and Footer Components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Header from "./app/header/Header";
+import Footer from "./app/footer/Footer";
+
 function App() {
-  return (
-    <div>
-      <h1>Hello World Admin</h1>
-    </div>
-  );
+
+  // TODO: If the user is logged out then only show the login screen and nothing else.
+  // TODO: Have a redirect that if the user isn't logged in it'll redirect them to the Login Page
+    return (
+        <Router>
+            <Header />
+            <main>
+                <h1>Hello world</h1>
+            </main>
+            <Footer />
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/app/footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <footer className="footer bg-dark py-5">
+            <div className="container-fluid g-0 text-white">
+                <div className="d-flex justify-content-center align-items-center">
+                    Footer, Please style me soon.
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
