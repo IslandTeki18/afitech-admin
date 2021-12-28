d1459f2639f3db8d1ff791a77b7e793c5abedd79
landon mckell <landon.roney7923@gmail.com>
2021-12-28 00:17:15 -0700

    Task: Implemented redux to testimonial details page.

--- src/pages/testimonialDetailsPage/TestimonialDetailsPage.jsx
+++ src/pages/testimonialDetailsPage/TestimonialDetailsPage.jsx
@@ -1,87 +1,106 @@
-import React from "react";
+import { useEffect } from "react";
 import "./TestimonialDetailsPage.scss";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { detailTestimonial } from "../../actions/testimonial.actions";
+import Loader from "../../components/loader/Loader";
+import Message from "../../components/message/Message";
 
 const TestimonialDetailsPage = () => {
-    return (
-        <div className="dkTestimonialDetailsPage">
-            <div className="container fullScreen">
-                <div className="d-flex justify-content-between align-items-center py-3">
-                    <h2>Client Name Here</h2>
-                    <Link to="/project-list" className="btn btn-secondary">
-                        back
-                    </Link>
-                </div>
-                <div className="row">
-                    <div className="col-lg-6">
-                        <div className="mb-3">
-                            <img
-                                src="https://via.placeholder.com/700x400"
-                                alt="placeholder"
-                                className="img-fluid mb-3"
-                            />
-                        </div>
-                        <div className="d-flex justify-content-between align-items-center">
-                            <div className="d-flex">
-                                <h5 className="me-4">Name:</h5>
-                                <p>John Doe</p>
-                            </div>
-                            <div className="d-flex">
-                                <h5 className="me-4">Created:</h5>
-                                <p>DD/MM/YYYY</p>
-                            </div>
-                        </div>
-                        <div className="d-flex justify-content-between align-items-center">
-                            <div className="d-flex">
-                                <h5 className="me-4">Is Active:</h5>
-                                <p className="text-danger">False</p>
-                            </div>
-                            <div className="btn-group">
-                                <Link
-                                    to="/testimonial/edit"
-                                    className="btn btn-secondary"
-                                >
-                                    Edit
-                                </Link>
-                                <button className="btn btn-danger">
-                                    Delete
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-lg-6">
-                        <div className="d-flex mb-2">
-                            <h5 className="me-4">Position at Company:</h5>
-                            <p>Lorem Ipsom</p>
-                        </div>
-                        <div className="d-flex mb-2">
-                            <h5 className="me-4">Company Website:</h5>
-                            <p>Lorem Ipsom</p>
-                        </div>
-                        <div className="d-flex mb-2">
-                            <h5 className="me-4">Company Location:</h5>
-                            <p>Lorem Ipsom</p>
-                        </div>
-                        <div className="testimonialWrapper mb-4">
-                            <h5 className="mb-2"> Testimonail:</h5>
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur
-                                adipiscing elit, sed do eiusmod tempor
-                                incididunt ut labore et dolore magna aliqua. Ut
-                                enim ad minim veniam, quis nostrud exercitation
-                                ullamco laboris nisi ut aliquip ex ea commodox
-                                consequat. Duis aute irure dolor in
-                                reprehenderit in voluptate velit esse cillum
-                                dolore eu fugiat nulla pariatur. Excepteur sint
-                                occaecat cupidatat non proident, sunt in culpa
-                                qui officia deserunt mollit anim id est laborum.
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+  const dispatch = useDispatch();
+  const { id } = useParams();
+  const testimonialDetails = useSelector((state) => state.testimonialDetails);
+  const { loading, error, testimonial } = testimonialDetails;
+
+  useEffect(() => {
+    window.scrollTo(0, "smooth");
+    dispatch(detailTestimonial(id));
+  }, [dispatch, id]);
+  return (
+    <div className="dkTestimonialDetailsPage">
+      <div className="container fullScreen">
+        <div className="d-flex justify-content-between align-items-center py-3">
+          <h2>{testimonial.name}</h2>
+          <Link to="/project-list" className="btn btn-secondary">
+            back
+          </Link>
         </div>
-    );
+        <div className="row">
+          {error && <Message variant="danger">{error}</Message>}
+          {loading ? (
+            <Loader />
+          ) : (
+            <>
+              <div className="col-lg-6">
+                <div className="mb-3">
+                  <img
+                    src="https://via.placeholder.com/700x400"
+                    alt="placeholder"
+                    className="img-fluid mb-3"
+                  />
+                </div>
+                <div className="d-flex justify-content-between align-items-center">
+                  <div className="d-flex">
+                    <h5 className="me-4">Name:</h5>
+                    <p>{testimonial.name}</p>
+                  </div>
+                  <div className="d-flex">
+                    <h5 className="me-4">Created:</h5>
+                    <p>
+                      {new Date(testimonial.createdAt).toLocaleDateString(
+                        "en-US",
+                        {
+                          year: "numeric",
+                          month: "short",
+                          day: "2-digit",
+                        }
+                      )}
+                    </p>
+                  </div>
+                </div>
+                <div className="d-flex justify-content-between align-items-center">
+                  <div className="d-flex">
+                    <h5 className="me-4">Is Active:</h5>
+                    <p
+                      className={`text-${
+                        testimonial.isActive ? "success" : "danger"
+                      }`}
+                    >
+                      {testimonial.isActive ? "Yes" : "No"}
+                    </p>
+                  </div>
+                  <Link
+                    to={`/testimonial/${id}/edit`}
+                    className="btn btn-secondary"
+                  >
+                    Edit
+                  </Link>
+                </div>
+              </div>
+              <div className="col-lg-6">
+                <div className="d-flex mb-2">
+                  <h5 className="me-4">Position at Company:</h5>
+                  <p>{testimonial.clientCompanyPosition}</p>
+                </div>
+                <div className="d-flex mb-2">
+                  <h5 className="me-4">Company Website:</h5>
+                  <p>{testimonial.companyWebsite}</p>
+                </div>
+                <div className="d-flex mb-2">
+                  <h5 className="me-4">Company Location:</h5>
+                  <p>{testimonial.companyLocation}</p>
+                </div>
+                <div className="testimonialWrapper mb-4">
+                  <h5 className="mb-2"> Testimonail:</h5>
+                  <p>{testimonial.testimonial}</p>
+                </div>
+              </div>
+            </>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default TestimonialDetailsPage;
