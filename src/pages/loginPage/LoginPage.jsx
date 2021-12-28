5956e12ce32cd7eb61e6889a48ed1b7b8c14e246
landon mckell <landon.roney7923@gmail.com>
2021-12-28 09:23:16 -0700

    Refactor: Added loading icon to login page, and if error, a message alert for the user

--- src/pages/loginPage/LoginPage.jsx
+++ src/pages/loginPage/LoginPage.jsx
@@ -4,19 +4,22 @@ import { useSelector, useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
 import { login } from "../../actions/user.actions";
 import InputLabel from "../../components/inputLabel/InputLabel";
+import Loader from "../../components/loader/Loader";
+import Message from "../../components/message/Message";
 
 const LoginPage = () => {
   const dispatch = useDispatch();
   const history = useHistory();
   const userLogin = useSelector((state) => state.userLogin);
+  const { loading, error, userInfo } = userLogin;
   const [userName, setUserName] = useState("");
   const [password, setPassword] = useState("");
 
   useEffect(() => {
-    if (userLogin.userInfo) {
+    if (userInfo) {
       history.push("/dashboard");
     }
-  }, [history, userLogin.userInfo]);
+  }, [history, userInfo]);
 
   function onSubmitForm(e) {
     e.preventDefault();
@@ -30,45 +33,52 @@ const LoginPage = () => {
           <div className="bg-secondary col-lg-6 d-none d-xl-block" />
           <div className="fullScreen col-md-12 col-xl-6 d-flex align-items-center">
             <div className="container">
+              {error && <Message variant="danger">{error}</Message>}
               <div className="row">
-                <div className="col-md-9 col-lg-8 mx-auto">
-                  <div className="card p-md-5 shadow-lg border-0">
-                    <div className="card-body">
-                      <h1 className="text-center pb-md-3">AFI ADMIN</h1>
-                      <form onSubmit={onSubmitForm}>
-                        <div className="mb-4">
-                          <InputLabel
-                            type="text"
-                            labelText="Username"
-                            placeholder="Username..."
-                            id="loginUsernameInput"
-                            value={userName}
-                            onChange={(e) => setUserName(e.target.value)}
-                          />
-                        </div>
-                        <div className="mb-4">
-                          <InputLabel
-                            type="password"
-                            labelText="Password"
-                            placeholder="Password..."
-                            id="loginPasswordInput"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                          />
-                        </div>
-                        <div className="d-grid">
-                          <button
-                            className="btn btn-primary"
-                            disabled={!userName || !password}
-                            type="submit"
-                          >
-                            Login
-                          </button>
-                        </div>
-                      </form>
+                {loading ? (
+                  <div className="d-flex justify-content-center">
+                    <Loader />
+                  </div>
+                ) : (
+                  <div className="col-md-9 col-lg-8 mx-auto">
+                    <div className="card p-md-5 shadow-lg border-0">
+                      <div className="card-body">
+                        <h1 className="text-center pb-md-3">AFI ADMIN</h1>
+                        <form onSubmit={onSubmitForm}>
+                          <div className="mb-4">
+                            <InputLabel
+                              type="text"
+                              labelText="Username"
+                              placeholder="Username..."
+                              id="loginUsernameInput"
+                              value={userName}
+                              onChange={(e) => setUserName(e.target.value)}
+                            />
+                          </div>
+                          <div className="mb-4">
+                            <InputLabel
+                              type="password"
+                              labelText="Password"
+                              placeholder="Password..."
+                              id="loginPasswordInput"
+                              value={password}
+                              onChange={(e) => setPassword(e.target.value)}
+                            />
+                          </div>
+                          <div className="d-grid">
+                            <button
+                              className="btn btn-primary"
+                              disabled={!userName || !password}
+                              type="submit"
+                            >
+                              Login
+                            </button>
+                          </div>
+                        </form>
+                      </div>
                     </div>
                   </div>
-                </div>
+                )}
               </div>
             </div>
           </div>
