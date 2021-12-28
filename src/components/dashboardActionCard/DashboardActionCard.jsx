6ad1f5cdef5c918ff2a255f06746479e2210c8c9
landon mckell <landon.roney7923@gmail.com>
2021-12-28 08:55:13 -0700

    Refactor: minor clean up to code base and added minor styling to login page

--- src/components/dashboardActionCard/DashboardActionCard.jsx
+++ src/components/dashboardActionCard/DashboardActionCard.jsx
@@ -3,45 +3,31 @@ import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 
 const DashboardActionCard = (props) => {
-    return (
-        <div className="dkDashboardActionCard">
-            <div className="card">
-                <div className="card-header bg-primary">
-                    <h4 className="text-white">{props.cardTitle}</h4>
-                </div>
-                <div className="card-body">
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-8">
-                                <div className="mb-3">
-                                    <Link
-                                        to={props.listViewUrl || "/dashboard"}
-                                    >
-                                        View {props.linkLabel || "Null"} List
-                                    </Link>
-                                </div>
-                                <div className="mb-3">
-                                    <Link
-                                        to={props.createViewUrl || "/dashboard"}
-                                    >
-                                        Create {props.linkLabel || "Null"}
-                                    </Link>
-                                </div>
-                            </div>
-                            <div className="col-4"></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="dkDashboardActionCard">
+      <div className="card">
+        <div className="card-header bg-primary">
+          <h4 className="text-white">{props.cardTitle}</h4>
         </div>
-    );
+        <div className="card-body">
+          <div className="container">
+            <div className="mb-3">
+              <Link to={props.listViewUrl || "/dashboard"}>
+                View {props.linkLabel || "Null"} List
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 DashboardActionCard.propTypes = {
-    cardTitle: PropTypes.string,
-    listViewUrl: PropTypes.string,
-    createViewUrl: PropTypes.string,
-    linkLabel: PropTypes.string,
+  cardTitle: PropTypes.string,
+  listViewUrl: PropTypes.string,
+  createViewUrl: PropTypes.string,
+  linkLabel: PropTypes.string,
 };
 
 export default DashboardActionCard;
--- src/pages/loginPage/LoginPage.jsx
+++ src/pages/loginPage/LoginPage.jsx
@@ -6,89 +6,76 @@ import { login } from "../../actions/user.actions";
 import InputLabel from "../../components/inputLabel/InputLabel";
 
 const LoginPage = () => {
-    const dispatch = useDispatch();
-    const history = useHistory();
-    const userLogin = useSelector((state) => state.userLogin);
-    const [userName, setUserName] = useState("");
-    const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const userLogin = useSelector((state) => state.userLogin);
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
 
-    useEffect(() => {
-        if (userLogin.userInfo) {
-            history.push("/dashboard");
-        }
-    }, [history, userLogin.userInfo]);
-
-    function onSubmitForm(e) {
-        e.preventDefault();
-        dispatch(login(userName, password));
-        history.push("/dashboard");
+  useEffect(() => {
+    if (userLogin.userInfo) {
+      history.push("/dashboard");
     }
-    return (
-        <div className="rsLoginPage">
-            <div className="container-fluid ps-md-0">
-                <div className="row g-0">
-                    <div className="bg-secondary col-lg-6 d-none d-xl-block" />
-                    <div className="fullScreen col-md-12 col-xl-6 d-flex align-items-center">
-                        <div className="container">
-                            <div className="row">
-                                <div className="col-md-9 col-lg-8 mx-auto">
-                                    <div className="card p-md-5">
-                                        <div className="card-body">
-                                            <h1 className="text-center pb-md-3">
-                                                Welcome back!
-                                            </h1>
-                                            <form onSubmit={onSubmitForm}>
-                                                <div className="mb-4">
-                                                    <InputLabel
-                                                        type="text"
-                                                        labelText="Username"
-                                                        placeholder="Username..."
-                                                        id="loginUsernameInput"
-                                                        value={userName}
-                                                        onChange={(e) =>
-                                                            setUserName(
-                                                                e.target.value
-                                                            )
-                                                        }
-                                                    />
-                                                </div>
-                                                <div className="mb-4">
-                                                    <InputLabel
-                                                        type="password"
-                                                        labelText="Password"
-                                                        placeholder="Password..."
-                                                        id="loginPasswordInput"
-                                                        value={password}
-                                                        onChange={(e) =>
-                                                            setPassword(
-                                                                e.target.value
-                                                            )
-                                                        }
-                                                    />
-                                                </div>
-                                                <div className="d-grid">
-                                                    <button
-                                                        className="btn btn-primary"
-                                                        disabled={
-                                                            !userName ||
-                                                            !password
-                                                        }
-                                                        type="submit"
-                                                    >
-                                                        Login
-                                                    </button>
-                                                </div>
-                                            </form>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+  }, [history, userLogin.userInfo]);
+
+  function onSubmitForm(e) {
+    e.preventDefault();
+    dispatch(login(userName, password));
+    history.push("/dashboard");
+  }
+  return (
+    <div className="rsLoginPage">
+      <div className="container-fluid ps-md-0">
+        <div className="row g-0">
+          <div className="bg-secondary col-lg-6 d-none d-xl-block" />
+          <div className="fullScreen col-md-12 col-xl-6 d-flex align-items-center">
+            <div className="container">
+              <div className="row">
+                <div className="col-md-9 col-lg-8 mx-auto">
+                  <div className="card p-md-5 shadow-lg border-0">
+                    <div className="card-body">
+                      <h1 className="text-center pb-md-3">AFI ADMIN</h1>
+                      <form onSubmit={onSubmitForm}>
+                        <div className="mb-4">
+                          <InputLabel
+                            type="text"
+                            labelText="Username"
+                            placeholder="Username..."
+                            id="loginUsernameInput"
+                            value={userName}
+                            onChange={(e) => setUserName(e.target.value)}
+                          />
                         </div>
+                        <div className="mb-4">
+                          <InputLabel
+                            type="password"
+                            labelText="Password"
+                            placeholder="Password..."
+                            id="loginPasswordInput"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                          />
+                        </div>
+                        <div className="d-grid">
+                          <button
+                            className="btn btn-primary"
+                            disabled={!userName || !password}
+                            type="submit"
+                          >
+                            Login
+                          </button>
+                        </div>
+                      </form>
                     </div>
+                  </div>
                 </div>
+              </div>
             </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
